c5d4f876393fcc33addc230882bf16144d6465af
Yunck02 <soupramanienyannick@gmail.com>
2025-12-19 14:07:36 +0100

    Starting the gold
    starting the gold counter system

---
+++ Assets/Scripts/GoldCounter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GoldCounter : MonoBehaviour
+{
+
+    public int GoldCount = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
